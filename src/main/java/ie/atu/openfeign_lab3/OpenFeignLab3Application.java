092cd6c430a34728523bf6efc9805759bb129b13
par2hibATU <g00410769@atu.ie>
2024-09-30 16:53:11 +0100

    First Commit

---
+++ src/main/java/ie/atu/openfeign_lab3/OpenFeignLab3Application.java
@@ -0,0 +1,15 @@
+package ie.atu.openfeign_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class OpenFeignLab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignLab3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/openfeign_lab3/OpenFeignLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.openfeign_lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignLab3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
