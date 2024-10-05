f8e1b620b87cab2d876ac7a30ed8788112f54c6d
par2hibATU <parthibbdcox@gmail.com>
2024-10-05 03:35:43 +0100

    This microservice is connected with the first one which I created on the week 2

--- src/main/java/ie/atu/openfeign_lab3/OpenFeignLab3Application.java
+++ src/main/java/ie/atu/openfeign_lab3/OpenFeignLab3Application.java
@@ -3,9 +3,11 @@ package ie.atu.openfeign_lab3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.validation.annotation.Validated;
 
 @EnableFeignClients
 @SpringBootApplication
+
 public class OpenFeignLab3Application {
 
     public static void main(String[] args) {
